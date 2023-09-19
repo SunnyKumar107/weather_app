ca8aef23b489644cb8cffc9bf28d9d5ab495fdc2
SunnyKumar107 <goodluck14115@gmail.com>
2023-09-19 22:38:57 +0500

    Set weather icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchBtn = document.querySelector(".search button");
 searchBtn.addEventListener("click", () => {
   const searchBox = document.getElementById("txt");
   console.log(searchBox.value);
+  const weatherIcon = document.querySelector(".weather_icon");
 
   const apiKey = "0bec87d89e84ef7429f80b60157e766e";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${
@@ -20,15 +21,28 @@ searchBtn.addEventListener("click", () => {
       Math.round(data.main.humidity) + " %";
     document.querySelector(".wind").innerHTML =
       Math.round(data.wind.speed) + " km/h";
+
+    console.log(data);
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "photo/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "photo/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "photo/rain.png";
+    } else if (data.weather[0].main == "Smoke") {
+      weatherIcon.src = "photo/smoke.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "photo/drizzle.png";
+    }
   }
 
   checkWeather();
 });
 
-// searchBox.addEventListener("input", function () {
-//   var cityName = this.value;
+// const txt = document.getElementById("txt");
 
-//   console.log(cityName);
+// txt.addEventListener("input", function () {
+//   let txtValue = this.value;
+//   console.log(txtValue);
 // });
-
-// console.log(searchBox);
